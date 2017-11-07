2ecd0ef7436e6d88a41388d0a8845592dbf97839
thomas <thomas.gerstenberg@novoengineering.com>
2017-11-06 19:07:02 -0800

    adding license, updating setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 
 from setuptools import setup, find_packages
-from os import path
+import sys
 
 
 VERSION = "v0.1.0"
+
+py2 = sys.version_info[0] == 2
+py3 = sys.version_info[0] == 3
+
+_install_requires = ["pc-ble-driver-py"]
+
+if py2:
+    _install_requires.extend(["enum34"])
 
 
 setup(
@@ -11,9 +19,8 @@ setup(
     version=VERSION.lstrip("v"),  # Remove the leading v, pip doesn't like that
     description="API for controlling nRF52 connectivity devices through pc-ble-driver-py",
     url="https://github.com/ThomasGerstenberg/blatann",
+    author="Thomas Gerstenberg",
+    keywords="ble bluetooth nrf52 nordic",
     packages=find_packages(exclude=["test", "test.*"]),
-    install_requires=[
-        "pc-ble-driver-py",
-        "enum34"
-    ],
+    install_requires=_install_requires,
 )
