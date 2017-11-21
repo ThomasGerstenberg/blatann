e64f812ce90cae8735116bf1048c9850e21ea229
thomas <thomas.gerstenberg@novoengineering.com>
2017-11-20 17:05:47 -0800

    moving the setup logger functionality to utils package

--- blatann/examples/example_utils.py
+++ blatann/examples/example_utils.py
@@ -1,18 +1,4 @@
-import logging
-import sys
-
-
-LOG_FORMAT = "[%(asctime)s] [%(threadName)s] [%(name)s.%(funcName)s:%(lineno)s] [%(levelname)s]: %(message)s"
-
-
-def setup_logger(name=None, level="DEBUG"):
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    formatter = logging.Formatter(LOG_FORMAT)
-    stream_handler = logging.StreamHandler(sys.stderr)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
-    return logger
+from blatann.utils import setup_logger
 
 
 def find_target_device(ble_device, name):
