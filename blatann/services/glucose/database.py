a1b65b7a3610395a562f5bcbf9ad44f3a5a79e7c
thomas <thomas.gerstenberg@novoengineering.com>
2023-09-28 21:49:49 -0700

    Adds type hints to glucose database

--- blatann/services/glucose/database.py
+++ blatann/services/glucose/database.py
@@ -1,5 +1,7 @@
 import logging
 from threading import RLock
+from typing import Optional
+
 from blatann.services.glucose.data_types import GlucoseMeasurement
 from blatann.services.glucose.racp import RacpResponseCode
 
@@ -11,25 +13,23 @@ class IGlucoseDatabase(object):
     """
     Defines the interface required for the Glucose Service to fetch records and record info
     """
-    def first_record(self):
+    def first_record(self) -> Optional[GlucoseMeasurement]:
         """
         Gets the first (oldest) record in the database
 
         :return: The first record in the database, or None if no records in the database
-        :rtype: GlucoseMeasurement
         """
         raise NotImplementedError()
 
-    def last_record(self):
+    def last_record(self) -> Optional[GlucoseMeasurement]:
         """
         Gets the last (newest) record in the database
 
         :return: The last record in the database, or None if no records in the database
-        :rtype: GlucoseMeasurement
         """
         raise NotImplementedError()
 
-    def record_count(self, min_seq_num=None, max_seq_num=None):
+    def record_count(self, min_seq_num: int = None, max_seq_num: int = None) -> int:
         """
         Gets the number of records between the minimum and maximum sequence numbers provided.
         The min/max limits are inclusive.
@@ -41,7 +41,7 @@ class IGlucoseDatabase(object):
         """
         raise NotImplementedError()
 
-    def get_records(self, min_seq_num=None, max_seq_num=None):
+    def get_records(self, min_seq_num: int = None, max_seq_num: int = None) -> list[GlucoseMeasurement]:
         """
         Gets a list of the records between the minimum sequence and maximum sequence numbers provided.
         The min/max limits are inclusive.
@@ -49,11 +49,10 @@ class IGlucoseDatabase(object):
         :param min_seq_num: The minimum sequence number to get. If None, no minimum is requested
         :param max_seq_num: The maximum sequence number to get. If None, no maximum is requested
         :return: The list of glucose measurement records that fit the parameters
-        :rtype: list[GlucoseMeasurement]
         """
         raise NotImplementedError()
 
-    def delete_records(self, min_seq_num=None, max_seq_num=None):
+    def delete_records(self, min_seq_num: int = None, max_seq_num: int = None) -> RacpResponseCode:
         """
         Deletes the records between the minimum sequence and maximum sequence numbers provided.
         The min/max limits are inclusive.
@@ -61,7 +60,6 @@ class IGlucoseDatabase(object):
         :param min_seq_num: The minimum sequence number to get. If None, no minimum is requested
         :param max_seq_num: The maximum sequence number to get. If None, no maximum is requested
         :return: The response code to send back for the operation
-        :rtype: RacpResponseCode
         """
         raise NotImplementedError()
 
@@ -72,7 +70,7 @@ class BasicGlucoseDatabase(IGlucoseDatabase):
     new records to the database.
     """
     def __init__(self, init_records=None):
-        self._records = []  # type: list[GlucoseMeasurement]
+        self._records: list[GlucoseMeasurement] = []
         if init_records is not None:
             self._records = init_records
         self._lock = RLock()
@@ -146,12 +144,11 @@ class BasicGlucoseDatabase(IGlucoseDatabase):
         logger.info("Glucose DB: Last record requested: {}".format(record))
         return record
 
-    def add_record(self, glucose_measurement):
+    def add_record(self, glucose_measurement: GlucoseMeasurement):
         """
         Adds a record to the database. NOTE: the measurement's sequence number must be unique within the database
 
         :param glucose_measurement: The measurement to add
-        :type glucose_measurement: GlucoseMeasurement
         """
         with self._lock:
             if glucose_measurement.sequence_number in [r.sequence_number for r in self._records]:
